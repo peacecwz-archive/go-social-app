1d67c940c7c26d126f722c57efea007fc7d05705
Baris Ceviz <me@barisceviz.com>
2018-04-29 23:54:22 +0300

    Added bash installer

--- install.sh
+++ install.sh
@@ -3,16 +3,21 @@
 #!/bin/sh
 echo "Hello, Installing dependencies for Go Social Web & Mobile App"
 echo "---------------------------------------------------------------"
+echo ""
+
 echo "Installing Go Iris Framework (github.com/kataras/iris)"
 go get -u github.com/kataras/iris
-echo "Installed"
-
+echo "Installed Go Iris" 
 echo "---------------------------------------------------------------"
+echo ""
+
 echo "Installing Go ORM (github.com/jinzhu/gorm)"
 go get -u github.com/jinzhu/gorm
-echo "Installed"
+echo "Installed Go ORM Package"
 
 echo "---------------------------------------------------------------"
+echo ""
+
 read -p "What's your DBMS Provider? (postgresql,mysql,mssql etc...): " dbProvider
 
 case "$dbProvider" in
@@ -36,6 +41,37 @@ case "$dbProvider" in
     echo "Cannot find your Database Provider. Please install Database Provider Driver manuelly"
     ;;
 esac
-
 echo "---------------------------------------------------------------"
+echo ""
+
+echo "Installing Go bcrypt"
+go get -u golang.org/x/crypto/bcrypt
+echo "Installed bcrypt"
+echo "---------------------------------------------------------------"
+echo ""
+
+echo "Installing Go Sessions"
+go get -u github.com/gorilla/sessions
+echo "Installed Go Sessions"
+echo "---------------------------------------------------------------"
+echo ""
+
+echo "Installing Godotenv"
+go get -u github.com/joho/godotenv
+echo "Installed Godotenv"
+echo "---------------------------------------------------------------"
+echo ""
+
+echo "Installing Checkmail"
+go get -u github.com/badoux/checkmail
+echo "Installed Checkmail"
+echo "---------------------------------------------------------------"
+echo ""
+
+echo "Installing npm packages"
+npm install
+echo "Installed npm packages"
+echo "---------------------------------------------------------------"
+echo ""
+
 echo "Installed all packages successfully"
